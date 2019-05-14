1141e22abab5342f49b6c630eb3ee814682df923
Jenczu11 <bartek.jencz@wp.pl>
2019-05-14 14:28:57 +0200

    test3

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("s");
         System.out.println("test");
+        System.out.println("test3");
     }
 }
