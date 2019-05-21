5646ff1d9b448f65779773eb2de6173b3dc3d2f3
Jenczu11 <bartek.jencz@wp.pl>
2019-05-21 14:19:48 +0200

    Added getters and Setters

--- src/main/java/Data/Event.java
+++ src/main/java/Data/Event.java
@@ -26,4 +26,44 @@ public class Event {
      * Zmienna informujaca, czy wystapi� ju� alarm przypisany do danego wydarzenia
      */
     public boolean alarmed;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
