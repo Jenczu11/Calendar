239f6c3c4361a94a00c092de861951e352f7e8b8
Jenczu11 <bartek.jencz@wp.pl>
2019-05-24 01:26:47 +0200

    Dodanie metod do DataService, removeEvent i daj wszystkie wydarzenia dla dnia Dodano testy jednostkowe

--- src/main/java/service/DataService.java
+++ src/main/java/service/DataService.java
@@ -6,6 +6,8 @@ import data.Event;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Calendar;
 
 public class DataService {
     private static DataService ourInstance = new DataService();
@@ -22,29 +24,29 @@ public class DataService {
     /**
      * Dodaje wydarzenie o podanych parametrach do repozytorium
      * @param id Id wydarzenia
-     * @param title Tytu� wydarzenia
+     * @param title Tytul wydarzenia
      * @param description Opis wydarzenia
-     * @param startDate Data rozpocz�cia wydarzenia
-     * @param endDate Data zako�czenia wydarzenia
-     * @throws Exception Je�li ju� istnieje zdarzenie o podanym id,
-     *  lub je�li data zako�czenia zdarzenia jest wcze�niejsza od daty rozpocz�cia zdarzenia
+     * @param startDate Data rozpoczecia wydarzenia
+     * @param endDate Data zakonczenia wydarzenia
+     * @throws Exception Jezeli juz istnieje zdarzenie o podanym id,
+     *  lub jezli data zakonczenia zdarzenia jest wczesniejsza od daty rozpoczecia zdarzenia
      */
     public void addEvent(String id,String title, String description, Timestamp startDate, Timestamp endDate) throws Exception {
         for (Event event : repository.getAllEvents()) {
             if(event.getId()==Integer.parseInt(id))
-                throw new Exception("Zdarzenie o takim id ju� istnieje");
+                throw new Exception("Zdarzenie o takim id juz istnieje");
             if(startDate.getTime()>endDate.getTime())
-                throw new Exception("Data zako�czenia zdarzenia nie mo�e by� wcze�niejsza od rozpocz�cia");
+                throw new Exception("Data zakonczenia zdarzenia nie moze byc wczesniejsza od rozpoczecia");
         }
         repository.addEvent(new Event(Integer.parseInt(id), title, description, startDate, endDate));
     }
     /**
      * Edytuje wydarzenie o podanych parametrach
      * @param id Id wydarzenia
-     * @param title Tytu� wydarzenia
+     * @param title Tytul wydarzenia
      * @param description Opis wydarzenia
-     * @param startDate Data rozpocz�cia wydarzenia
-     * @param endDate Data zako�czenia wydarzenia
+     * @param startDate Data rozpoczecia wydarzenia
+     * @param endDate Data zakonczenia wydarzenia
      * @throws Exception Kiedy wydarzenie o podanym id nie istnieje
      */
     public void editEvent(String id, String title, String description, Timestamp startDate, Timestamp endDate) throws Exception {
@@ -64,16 +66,55 @@ public class DataService {
      * @throws Exception Kiedy nie istnieje wydarzenie o podanym id
      */
     public void removeEvent(String id) throws Exception {
-        int idInt=Integer.parseInt(id);
+        int idInt = Integer.parseInt(id);
         for (Event event : repository.getAllEvents()) {
-            if(event.getId()==idInt) {
+            if (event.getId() == idInt) {
                 repository.removeEvent(event);
                 return;
             }
         }
         throw new Exception("Zdarzenie nie istnieje");
     }
+        /**
+         * Usuwa wydarzenie o podanym id z repozytorium
+         * @param id Id wydarzenia
+         * @throws Exception Kiedy nie istnieje wydarzenie o podanym id
+         */
+        public void removeEvent(int id) throws Exception {
+            for (Event event : repository.getAllEvents()) {
+                if (event.getId() == id) {
+                    repository.removeEvent(event);
+                    return;
+                }
+            }
+            throw new Exception("Zdarzenie nie istnieje");
+        }
+    /**
+     * Zwraca wszystkie eventy dla danej daty
+     * @param stamp Data dla ktorej sa szukane Zdarzenia
+     * @return Lista zdarzen dla danej daty
+     */
+     public List<Event> GetAllEventsForDate(Timestamp stamp){
+        List<Event> Todays = new ArrayList<Event>();
+        for(int i=0;i<repository.size();i++) {
+            long timestamp = repository.getAllEvents().get(i).getStartDate().getTime();
+            Calendar oneFromAll = Calendar.getInstance();
+            oneFromAll.setTimeInMillis(timestamp);
 
+            Calendar selected= Calendar.getInstance();
+            selected.setTime(stamp);
+
+            if(oneFromAll.get(Calendar.MONTH)==selected.get(Calendar.MONTH) && oneFromAll.get(Calendar.YEAR)==selected.get(Calendar.YEAR) && oneFromAll.get(Calendar.DAY_OF_MONTH)==selected.get(Calendar.DAY_OF_MONTH))
+            {
+                Todays.add(repository.getAllEvents().get(i));
+            }
+        }
+        return Todays;
+    }
+    public int size()
+    {
+        return repository.size();
+    }
     @Override
     public String toString() {
         return "DataService{" +
--- src/test/java/service/DataServiceTest.java
+++ src/test/java/service/DataServiceTest.java
@@ -1,9 +1,12 @@
 package service;
 
 import data.Event;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,12 +15,66 @@ class DataServiceTest {
     @Test
     void addEvent() {
         DataService dataService = DataService.getInstance();
+        createTestData(dataService);
+        assertEquals(4,dataService.size());
+        System.out.println(dataService.toString());
+
+    }
+
+    @Test
+    void editEvent() {
+        DataService dataService = DataService.getInstance();
         try {
             dataService.addEvent("1","Title1","Title2", Timestamp.valueOf("2019-05-15 18:48:00"),Timestamp.valueOf("2019-05-16 18:48:00"));
         } catch (Exception e) {
             e.printStackTrace();
         }
         System.out.println(dataService.toString());
+    }
+
+    @Test
+    @DisplayName("Czy poprawnie usuwa elementy")
+    void removeEvent() throws Exception {
+        DataService dataService = DataService.getInstance();
+        createTestData(dataService);
+        assertEquals(4,dataService.size(),"Repository size equals 3");
+        dataService.removeEvent("2");
+        System.out.println(dataService.toString());
+        assertEquals(3,dataService.size());
+        dataService.removeEvent(4);
+        System.out.println(dataService.toString());
+        assertEquals(2,dataService.size());
+    }
+    @Test
+    @DisplayName("Czy rzuca wyjatkiem o nie istnejacym zdarzeniu")
+    void isThrowingExceptionForNonExistingEvent()
+    {
+        DataService dataService = DataService.getInstance();
+        createTestData(dataService);
+        assertThrows(Exception.class,()->dataService.removeEvent(5));
+
+    }
+    void createTestData(DataService dataService)
+    {
+        try {
+            dataService.addEvent("1","Title1","Title2", Timestamp.valueOf("2019-05-15 18:48:00"),Timestamp.valueOf("2019-05-16 18:48:00"));
+            dataService.addEvent("2","Title1","Title2", Timestamp.valueOf("2019-05-15 18:48:00"),Timestamp.valueOf("2019-05-16 18:48:00"));
+            dataService.addEvent("3","Title1","Title2", Timestamp.valueOf("2019-05-15 18:48:00"),Timestamp.valueOf("2019-05-16 18:48:00"));
+            dataService.addEvent("4","Title1","Title2", Timestamp.valueOf("2019-05-13 18:48:00"),Timestamp.valueOf("2019-05-16 18:48:00"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Test
+    void getAllEventsForDate() {
+        DataService dataService = DataService.getInstance();
+        createTestData(dataService);
+        List<Event> today = dataService.GetAllEventsForDate(Timestamp.valueOf("2019-05-15 00:00:00"));
+        System.out.println(today.toString());
+        assertEquals(3,today.size());
 
     }
 }
