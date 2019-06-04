698770f12bfd9312f35da3cce57931d7a75b5708
Jenczu11 <bartek.jencz@wp.pl>
2019-06-04 12:56:34 +0200

    Opisanie wyjatkow

--- src/main/java/exceptions/dataException.java
+++ src/main/java/exceptions/dataException.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+/**
+ * Klasa odpowiadajaca za wyjatek zwiazany z timestampem
+ */
 public class dataException extends Exception {
     public dataException(String s) {
         super(s);
--- src/main/java/exceptions/idException.java
+++ src/main/java/exceptions/idException.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+/**
+ * Klasa odpowiadajaca za wyjatek zwiazany z ID eventu
+ */
 public class idException extends Exception {
     public idException(String s) {
         super(s);
