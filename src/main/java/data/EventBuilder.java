2d72e1b649f6007ed95587d4670c69de8d2a301f
Jenczu11 <bartek.jencz@wp.pl>
2019-06-04 11:21:07 +0200

    Przed refactoringiem

--- src/main/java/data/EventBuilder.java
+++ src/main/java/data/EventBuilder.java
@@ -2,45 +2,71 @@ package data;
 
 import java.sql.Timestamp;
 
+/**
+ * Klasa implementujaca wzorzec projektowy budowniczego
+ * Pozwala na tworzenie eventow z ich wartosciami domyslnymi
+ */
 public class EventBuilder {
-    //TODO: zmienić to po testach na private
+
+    /**
+     * Id wydarzenia
+     */
     private int id;
+    /**
+     * Tytul wydarzenia
+     */
     private String title;
-    private String description;
+    /**
+     * Miejsce wydarzenia
+     */
+    private String place;
+    /**
+     * Data rozpoczecia wydarzenia
+     */
     private Timestamp startDate;
+    /**
+     * Data zakonczenia wydarzenia
+     */
     private Timestamp endDate;
+    /**
+     * Zmienna informujaca, czy wystapil juz alarm przypisany do danego wydarzenia
+     */
     private boolean alarm;
 
 
+    /**
+     * Ustawia id wydarzenia
+     * @param id Id wydarzenia
+     * @return EventBuilder
+     */
     public EventBuilder setId(int id) {
-
         this.id = id;
         return this;
     }
+
     public EventBuilder setId(String idValue) {
         //TODO: nad tym trzeba popracowac bo funkcja zle zadziala kiedy posortujemy dane i bedziemy chcieli wziac ostatnie ID
         //
         //Bezpieczniej moze byc poprostu branie tablicy ale to do zmiany po konsultacjach
 
-        if(idValue.isEmpty()) {
+        if (idValue.isEmpty()) {
             //            idValue=Integer.toString((int)Math.random()*100);
-             this.id= DataRepository.getLastEventID()+1;
-        }
-        else this.id = Integer.parseInt(idValue);
+            this.id = DataRepository.getLastEventID() + 1;
+        } else this.id = Integer.parseInt(idValue);
         return this;
     }
 
     public EventBuilder setTitle(String title) {
-        if(title.isEmpty())
-            title="Default Title";
+        if (title.isEmpty())
+            title = "Default Title";
         this.title = title;
         return this;
     }
 
     public EventBuilder setPlace(String place) {
-        if(place.isEmpty())
-            place ="Default Place";
-        this.description = place;
+        if (place.isEmpty())
+            place = "Default Place";
+        this.place = place;
         return this;
     }
 
@@ -55,13 +81,14 @@ public class EventBuilder {
         this.endDate = endDate;
         return this;
     }
+
     public EventBuilder setAlarm(boolean alarm) {
         this.alarm = alarm;
         return this;
     }
 
     public Event createEvent() {
-        return new Event(id, title, description, startDate, endDate);
+        return new Event(id, title, place, startDate, endDate);
     }
 
     @Override
@@ -69,7 +96,7 @@ public class EventBuilder {
         return "Wydarzenie{" +
                 "id=" + id +
                 ", Tyt='" + title + '\'' +
-                ", Des='" + description + '\'' +
+                ", Des='" + place + '\'' +
                 ", start=" + startDate +
                 ", end=" + endDate +
                 ", alarm=" + alarm +
