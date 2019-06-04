95fbabf64a8396bb1b9278f5ccf99ac983701fd5
Jenczu11 <bartek.jencz@wp.pl>
2019-06-04 10:58:21 +0200

    Dodanie GoogleCalendarHandlera

---
+++ src/main/java/service/GoogleCalendarHandler.java
@@ -0,0 +1,80 @@
+package service;
+
+import data.DataRepository;
+import data.Event;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class GoogleCalendarHandler implements IOHandler  {
+    /**
+     * Nazwa pliku, do ktorego zostaja zapisane dane
+     */
+    private String filename;
+    /**
+     * Ustawia nazwe pliku, do ktorego zostana zapisane dane
+     * @param filename Nazwa pliku
+     */
+    public void setFilename(String filename) {
+        this.filename=filename;
+    }
+
+    public GoogleCalendarHandler(String filename) {
+        if(filename.isEmpty())
+            filename="ExportGCalendar";
+        filename+=".csv";
+        this.filename = filename;
+    }
+
+    /**
+     * Laduje dane z plikow csv, nie zaimplementowane i nie wymagane
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public DataRepository LoadData() throws Exception {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    /**
+     * Zapisuje pliki do pliku csv okreslonego konstruktorem/lub setterem
+     * @param data , zawierajace liste wydarzen
+     * @throws Exception
+     */
+    @Override
+    public void SaveData(DataRepository data) throws Exception {
+        PrintWriter pw = new PrintWriter(new File(filename));
+        StringBuilder sb = new StringBuilder();
+        sb.append("Subject");
+        sb.append(',');
+        sb.append("Start Date");
+        sb.append(',');
+        sb.append("Start Time");
+        sb.append(',');
+        sb.append("End Date");
+        sb.append(',');
+        sb.append("End Time");
+        sb.append(',');
+        sb.append("Place");
+        sb.append('\n');
+        for (Event event : data.getAllEvents()) {
+            sb.append(event.getTitle());
+            sb.append(',');
+            sb.append(new SimpleDateFormat("MM/dd/yyyy").format(event.getStartDate()));
+            sb.append(',');
+            sb.append(new SimpleDateFormat("KK:mm aa",Locale.ENGLISH).format(event.getStartDate()));
+            sb.append(',');
+            sb.append(new SimpleDateFormat("MM/dd/yyyy").format(event.getEndDate()));
+            sb.append(',');
+            sb.append(new SimpleDateFormat("KK:mm aa", Locale.ENGLISH).format(event.getEndDate()));
+            sb.append(',');
+            sb.append(event.getPlace());
+            sb.append('\n');
+        }
+        pw.write(sb.toString());
+        pw.close();
+
+    }
+}
---
+++ src/test/java/service/GoogleCalendarHandlerTest.java
@@ -0,0 +1,48 @@
+package service;
+
+import data.EventBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GoogleCalendarHandlerTest {
+    @BeforeEach
+    public void resetSingleton() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        Field instance = DataService.class.getDeclaredField("ourInstance");
+        instance.setAccessible(true);
+        instance.set(null, null);
+    }
+
+
+    @ParameterizedTest(name = "For all empty strings it should dataRepo must have 10 events, and must save to XML")
+    @CsvSource({
+            "'', '', '','','','',''",
+    })
+    public void saveData(String id, String title, String description, String startDate, String endDate) {
+        DataService dataService = DataService.getInstance();
+        int expected;
+        for (expected = 0; expected < 10; expected++) {
+            EventBuilder builder = new EventBuilder();
+            try {
+                dataService.refreshLastEventID();
+                builder.setId(id);
+                builder.setTitle(title);
+                builder.setPlace(description);
+                builder.setStartDate(dataService.StringToTimestamp(startDate));
+                builder.setEndDate(dataService.StringToTimestamp(endDate));
+                dataService.addEvent(builder.createEvent());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        assertEquals(expected, dataService.getRepositoryEvents().size());
+        GoogleCalendarHandler gcalh = new GoogleCalendarHandler("GcalTest");
+        assertDoesNotThrow(() -> dataService.saveRepository(gcalh));
+
+    }
+}
